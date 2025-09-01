feb6af26d1a57f3d639f380f0520a2e43f183b50
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-01 16:14:50 -0400

    spelling: timevalues
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/nlu-engine/src/engine/entities/microsoft-extractor/index.ts
+++ packages/nlu-engine/src/engine/entities/microsoft-extractor/index.ts
@@ -75,19 +75,19 @@ export class MicrosoftEntityExtractor implements SystemEntityExtractor {
 
     if (entity.typeName.includes('datetimeV2')) {
       const resolution = entity.resolution as MicrosoftValues
-      const metadatas = resolution.values[0] as MicrosoftTimeValues
+      const timeValues = resolution.values[0] as MicrosoftTimeValues
 
-      unit = metadatas.type
+      unit = timeValues.type
       entity.typeName = DucklingDateMappings[entity.typeName]
 
       switch (entity.typeName) {
         case 'duration':
           // TODO Deal with intervals ! Also need to be fixed in duckling.
-          value = metadatas.start || metadatas.end!
+          value = timeValues.start || timeValues.end!
           break
 
         case 'time':
-          value = metadatas.value!
+          value = timeValues.value!
           break
 
         default:
